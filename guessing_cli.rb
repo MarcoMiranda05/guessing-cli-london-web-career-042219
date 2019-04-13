d2014839610ba5015df6c74264038f63be476379
Marco Miranda <marmirand@hotmail.com>
2019-04-13 10:40:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,3 +1,18 @@
 def run_guessing_game
+  puts "Guess a number between 1 and 5"
+  number = rand(1..5).to_s
+  user_answer = gets.chomp
 
+   while user_answer != "exit" do
+    if user_answer == number
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{number}."
+    end
+
+     puts "Guess a number between 1 and 6."
+    number = rand(1..6).to_s
+    user_answer = gets.chomp
+  end
+  puts "Goodbye!"
 end
